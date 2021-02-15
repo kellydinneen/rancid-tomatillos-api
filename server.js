e4b8b004bbb815f5f8658e9e5e9e52c0542a4d2d
Lucas Merchant <lbmerchant93@gmail.com>
2021-02-15 11:46:39 -0700

    Fix missing parenthesis

--- server.js
+++ server.js
@@ -49,7 +49,7 @@ app.patch('/api/v1/users/:id', (request, response) => {
     return response.sendStatus(404);
   }
 
-  if(!user.favorites.some(fav => fav.id === newFavorite.id){
+  if(!user.favorites.some(fav => fav.id === newFavorite.id)) {
    user.favorites.push(newFavorite)
   } else {
     user.favorites = user.favorites.filter(fav => fav.id !== newFavorite.id)
